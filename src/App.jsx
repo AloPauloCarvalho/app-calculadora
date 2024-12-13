00608f45d85d7451ff38bc8acd3f44a719c69b01
Paulo Carvalho <alopaulocarvalho@gmail.com>
2024-12-13 17:45:53 -0300

    handleOperationResult foi simplificado com o eval()

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,23 @@ function App() {
   function handleOperationInput(input) {
     setArrOperatorMemory((arr) => {
 
-      // 1- primeiro input não pode ser um operador
-      if ((arr.length === 0 && !Number.isInteger(input))) {
+      // 1- primeiro input não pode ser um operador, apenas '-' para representar um numero negativo
+      if ((arr.length === 0 && ['*', '/', '+'].includes(input))) {
         return arr;
       }
 
-      // 1.5 - se existir resultado de uma conta prévia e o input for um operador
-      if ((numOperationResult != 0 && !Number.isInteger(input))) {
+      // 1.5 - se existir resultado de uma conta prévia e o input for um operador (ver como não atualizar a memoria mesmo depois do =)
+      if ((numOperationResult && !Number.isInteger(input))) {
         return [numOperationResult, input];
       }
 
-      // 2- não podem existir dois operadores seguidos
-      if (!Number.isInteger(input) && !Number.isInteger(arr[arr.length - 1])) {
+      // 2- não podem existir dois operadores seguidos, ao menos q seja um '-' depois de '*' ou '/'
+      if (!Number.isInteger(input) && ['*', '/', '+'].includes(arr[arr.length - 1])) {
         const tempArr = [...arr];
         tempArr[tempArr.length - 1] = input;
         return tempArr;
       }
 
-
-      // NAO ESTA FUNCIONANDO QUANDO DÁ RODA DA OPERAÇÃO ELE AINDA CONCATENA NUMERO NA MEMORIA QUANDO ESSA PARTE DEVERIA ESTAR BLOQUEADA 
       // 3- caso o último input seja um número e input anterior também, concatena esse input com o número anterior
       if (Number.isInteger(input) && numResultSetted) {
         if (arr.length > 0 && Number.isInteger(arr[arr.length - 1])) {
@@ -46,25 +44,6 @@ function App() {
     console.log(arrOperatorMemory);
   }
 
-
-  //0 - função que calcula 
-  function calcular(x, y, operator) {
-    switch (operator) {
-      case '+':
-        return x + y;
-      case '-':
-        return x - y;
-      case '*':
-        return x * y;
-      case '/':
-        if (y === 0) {
-          //exibir erro no display de resultado
-          throw new Error('Err');
-        }
-        return x / y;
-    }
-  }
-
   // 0- função que executa a expressão armazenada no arrOperatorMemory
   function handleOperationResult(arr) {
     setNumResultSetted(true);
@@ -76,28 +55,7 @@ function App() {
         tempArr.pop();
       }
 
-      // 2- operadores de multiplicação e divisão
-      for (let i = 0; i < tempArr.length; i++) {
-        if (['*', '/'].includes(tempArr[i])) {
-          tempArr.splice(i - 1, 3, calcular(tempArr[i - 1], tempArr[i + 1], tempArr[i]));
-          i--;
-        }
-      }
-
-      // 3- operadores de soma e subtração
-      for (let i = 0; i < tempArr.length; i++) {
-        if (['+', '-'].includes(tempArr[i])) {
-          tempArr.splice(i - 1, 3, calcular(tempArr[i - 1], tempArr[i + 1], tempArr[i]));
-          i--;
-        }
-      }
-
-      // 4- retorna resultado tratando casos de float/dizima periodica
-      if (Number.isFinite(tempArr[0]) && tempArr[0] % 1 !== 0) {
-        return tempArr[0].toLocaleString(undefined, { maximumFractionDigits: 3, minimumFractionDigits: 0 });
-      } //roubei do stackoverflow e tá dando merda
-
-      return tempArr[0];
+      return eval(tempArr.toString().replaceAll(',', ''));
     })
   }
 
